fb803b1c5871b7465e6a6e9222419bbd29fdb79c
Uildez <uildesdacosta@gmail.com>
2022-04-28 16:42:51 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,87 @@
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
-import Login from './Components/Login/Login'
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from './features/userSlice';
 import Logout from './Components/Logout/Logout'
+import { useEffect, useState } from 'react';
+import { login } from './features/userSlice';
+import { Redirect } from 'react-router-dom';
+
+import { auth } from './firebase';
+import './Components/Login/login.scss';
+import { ReactComponent as Logo } from './assets/images/menew-logo.svg';
 
 function App() {
-
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const [isRegister, setIsRegister] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    isRegister ? handleLogin() : handleRegister()
+  };
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(login({
+          id: authUser.uid,
+          name: authUser.displayName ? authUser.displayName : authUser.email,
+        }));
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [])
+
+  const handleLogin = () => {
+    if (email && password !== "") {
+      auth.signInWithEmailAndPassword(email, password).then((data) => alert('Logado com sucesso')).catch((err) => alert((err)));
+    }
+  };
+
+  const handleRegister = () => {
+    if (email && password !== "") {
+      auth.createUserWithEmailAndPassword(email, password).then((data) => alert('Registrado com sucesso')).catch((err) => alert((err)));
+    }
+  };
 
   return (
     <div className='App'>
+      {""}
       {user ?
-        <Logout/> : <Login />}
+        <Logout/>
+        :
+        <section className='section-bg'>
+          <div className='login'>
+            <div className='login_logo'>
+              <Logo />
+            </div>
+            {isRegister ?
+              <>
+                <form className='login__form' onSubmit={(e) => handleSubmit(e)}>
+                  <h2>Acesso</h2>
+                  <input type="email" placeholder="Seu Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                  <input type="password" placeholder="Sua senha" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                  <button type="submit" className='submit__btn'>{isRegister ? "Entrar" : "Finalizar"}</button>
+                </form>
+              </> :
+              <>
+                <form className='login__form' onSubmit={(e) => handleSubmit(e)}>
+                  <h2>Cadastro</h2>
+                  <input type="email" placeholder="Seu Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                  <input type="password" placeholder="Sua senha" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                  <button type="submit" className='submit__btn'>{isRegister ? "Entrar" : "Finalizar"}</button>
+                </form>
+              </>
+            }
+            <p className='register-text'> {isRegister ? "Não tem cadastro? " : "Já tem cadastro? "}
+              <span onClick={() => setIsRegister((show) => !show)}>{isRegister ? "Realize o seu aqui" : "Faça seu login"}</span>
+            </p>
+          </div>
+        </section>}
     </div>
   );
 }
--- src/Components/Logout/Logout.js
+++ src/Components/Logout/Logout.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { logout, selectUser } from '../../features/userSlice'
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice'
 
 import './logout.scss'
 import { ReactComponent as Logo } from '../../assets/images/menew-logo.svg';
 import AppRoutes from '../../AppRoutes';
+import { auth } from '../../firebase';
 
 const Logout = () => {
   const user = useSelector(selectUser);
 
-  const dispatch = useDispatch();
-
-  const handleLogout = (e) => {
-    e.preventDefault();
-
-    dispatch(logout());
+  const handleLogout = () => {
+    if(window.confirm("Deseja realmente sair?")){
+      auth.signOut(); 
+    }
   }
 
   return (
@@ -23,13 +22,14 @@ const Logout = () => {
         <div className='logout'>
           <div className='logout_logo'>
             <Logo />
-            <h2>Bem vindo <span className='user__name'>{user.name}</span></h2>{" "}
+            <h2>Bem vindo <span className='user__name'>{String(user.name).split("@")[0]}</span></h2>{" "}
           </div>
           <div className='buttons'>
-            <button className='logout__btn' onClick={(e) => handleLogout(e)}>Sair</button>
+            <button className='logout__btn' onClick={handleLogout}>Sair</button>
           </div>
         </div>
       </div>
+      
       <AppRoutes />
     </div>
 
